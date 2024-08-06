51d453428014326db94452913de472303e931a64
Nathishsriganesh <151003529+Srinathish@users.noreply.github.com>
2024-08-06 08:48:24 +0530

    Add files via upload

---
+++ DAY 1/DAY_CY1.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+function compare()
+{
+    const name1="Kabil";
+    const name2="Kabil";
+    const object1={name:"Kabil"};
+    const object2={name:"Kabil"};
+    console.log(name1===name2);
+    console.log(object1===object2);
+    console.log(object1===name1);
+    console.log(object2===name2);
+    alert("Check the console!");
+}
+const Check = () => {
+    return(
+        <div>
+            <button onClick={compare}>CY1</button>
+        </div>
+    );
+};
+export default Check;
